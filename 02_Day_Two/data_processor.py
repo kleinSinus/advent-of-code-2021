f468cb9603f285d6426e45bbc786adefb5807aa1
kleinSinus <wilhelm.simus@gmail.com>
2021-12-02 20:55:15 +0100

    Add aim => Day 2 - Part 2 - Done

--- 02_Day_Two/data_processor.py
+++ 02_Day_Two/data_processor.py
@@ -1,15 +1,17 @@
 def process_movement(input_data):
     x = 0
     y = 0
+    aim = 0
     for command in input_data:
         direction = command.split(' ')[0]
         value = int(command.split(' ')[1])
         if direction == "forward":
             x += value
+            y += aim*value
         elif direction == "up":
-            y -= value
+            aim -= value
         elif direction == "down":
-            y += value
+            aim += value
         else:
             print("Direction " + direction + " was not expected")
     return [x, y]
