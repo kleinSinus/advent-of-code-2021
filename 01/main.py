a006716d1fe9d0559ac7f2c76651e30b08915079
kleinSinus <wilhelm.simus@gmail.com>
2021-12-02 16:54:10 +0100

    Measurements complete

--- 01/main.py
+++ 01/main.py
@@ -13,24 +13,43 @@ def measurement_difference_string(input_data, i):
 # process input data array with depth measurements, return table with measurements and their status
 def process_measurements(input_data):
     array_size = len(input_data)
-    status_messages = [0] * array_size
+    status_messages = [""] * array_size
     for i in range(array_size):
-        new_message = measurement_difference_string(input_data, i)
+        new_message = str(input_data[i]) + ", " + measurement_difference_string(input_data, i)
+        print(new_message)
         status_messages[i] = new_message
     return status_messages
 
 
-# process input data array with depth measurements, return table with measurements and their status
+# count increments within input data
 def count_increments(input_data):
     array_size = len(input_data)
     counter = 0
     for i in range(array_size-1):
         if input_data[i+1] > input_data[i]:
             counter += 1
+    print("\nIn this input data there are " + str(counter) + " measurements larger than the previous measurement.")
     return counter
 
 
-# start measurement processing for test data
-test_data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-process_measurements(test_data)
-count_increments(test_data)
+def process_input_file():
+    # get data
+    file = open("input.txt", "r")
+    input_data = []
+    for line in file:
+        content = int(line[:-1])
+        input_data.append(content)
+    print(input_data)
+    process_measurements(input_data)
+    count_increments(input_data)
+
+
+def process_test_input():
+    input_data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    process_measurements(input_data)
+    count_increments(input_data)
+
+
+# process_test_input()
+process_input_file()
+
